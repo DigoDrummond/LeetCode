6af0ee963b9d57b1df0004f9743b20f0fe125f74
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2023-11-05 17:55:51 -0300

    Add Two Numbers

---
+++ Add_Two_Numbers.java
@@ -0,0 +1,44 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Add_Two_Numbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode saida = new ListNode(0);
+        ListNode atual = saida;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            atual.next = node;
+            atual = atual.next;
+        }
+
+        return saida.next;
+    }
+}
